43e5b44d96e1feb584edf80ff3d0f23f7f834800
Chayma-05 <souaidichayma@gmail.com>
2024-05-05 01:47:53 +0100

    crud backend

---
+++ Backend/src/main/java/com/cinema/cine/repository/ProjectionRepository.java
@@ -0,0 +1,7 @@
+package com.cinema.cine.repository;
+
+import com.cinema.cine.entity.Projection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectionRepository extends JpaRepository<Projection, Integer> {
+}
